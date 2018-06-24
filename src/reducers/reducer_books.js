2321f89d2f7cd3b4c578d365d028b2746a3844f2
moose <mr.rammo@gmail.com>
2018-06-24 19:44:54 -0400

    Added selectable components with real time updates

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 let BooksReducer = function () {
     return [
-        {title: 'Javascript: The Good Parts'},
-        { title: 'Harry Potter' },
-        { title: 'Dark Towers' },
-        { title: 'Eloquent Ruby' },
+        { title: 'Javascript: The Good Parts', pages: 12},
+        { title: 'Harry Potter', pages: 39 },
+        { title: 'Dark Towers', pages: 90 },
+        { title: 'Eloquent Ruby', pages: 45 },
     ];
 }
 
