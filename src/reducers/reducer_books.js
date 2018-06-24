ec7d13f57b6a53eedc02821e52dc3acf73871058
moose <mr.rammo@gmail.com>
2018-06-24 19:02:17 -0400

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+let BooksReducer = function () {
+    return [
+        {title: 'Javascript: The Good Parts'},
+        { title: 'Harry Potter' },
+        { title: 'Dark Towers' },
+        { title: 'Eloquent Ruby' },
+    ];
+}
+
+export default BooksReducer;
